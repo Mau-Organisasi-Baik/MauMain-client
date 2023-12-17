7adc6b4d2858c21b0670e7bac197643a8ea4982e
Maulana Ibrahim <maulputra09@gmail.com>
2023-12-17 18:09:32 +0700

    feat: Successfully wired up login/register API on client

--- src/screens/AuthForm.js
+++ src/screens/AuthForm.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, TextInput, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRoute } from '@react-navigation/native';
 import { Picker } from '@react-native-picker/picker'
+import Toast  from 'toastify-react-native';
+import axios from 'axios';
 
 const AuthForm = ({navigation}) => {
     const {name} = useRoute()
     const loginScreen = (name === 'Login')
     const registerScreen = (name === 'Register')
+    const uri = registerScreen ? 'register' : 'login'
+    const [inputValues, setInputValues] = useState({
+      name : '',
+      username : '',
+      phoneNumber : '',
+      email : '',
+      password : '',
+      role : ''
+    })
+    const inputHandler = (inputIdentifier, enteredValue) => {
+      setInputValues(currValue => {
+        return {
+          ...currValue,
+          [inputIdentifier] : enteredValue
+        }
+        
+      })
+    }
+    
+    const submitForm = async () => {
+      try {
+        await axios.post(`https://3ff1-110-137-195-250.ngrok-free.app/${uri}`, inputValues)
+        if(registerScreen) {
+          navigation.navigate('Login')
+        } else {
+          navigation.navigate('Explore')
+        }
+      } catch (error) {
+        if (error.response) {
+          console.log(error.response.data.message);
+          Toast.error(error.response.data.message)
+        } else if (error.request) {
+          Toast.error(error.request);
+        } else {
+          Toast.error('Error', error.message);
+        }
+      }
+    }
+
   return (
     <LinearGradient
       colors={['#003366', '#66CCFF']}
@@ -17,18 +58,28 @@ const AuthForm = ({navigation}) => {
       
       <TextInput
         placeholder="Email"
+        onChangeText={inputHandler.bind(this, 'usernameOrMail' ||'email' )}
         placeholderTextColor="rgba(255, 255, 255, 0.7)"
         className="text-white text-base mb-4 px-2 py-2 border-b border-white rounded-md"
       />
       { registerScreen &&
       <TextInput
         placeholder="Username"
+        onChangeText={inputHandler.bind(this, 'username')}
+        placeholderTextColor="rgba(255, 255, 255, 0.7)"
+        className="text-white text-base mb-4 px-2 py-2 border-b border-white rounded-md"
+      /> }
+      { registerScreen &&
+      <TextInput
+        placeholder="Phone Number"
+        onChangeText={inputHandler.bind(this, 'phoneNumber')}
         placeholderTextColor="rgba(255, 255, 255, 0.7)"
         className="text-white text-base mb-4 px-2 py-2 border-b border-white rounded-md"
       /> }
       
       <TextInput
         placeholder="Password"
+        onChangeText={inputHandler.bind(this, 'password')}
         secureTextEntry={true}
         placeholderTextColor="rgba(255, 255, 255, 0.7)"
         className="text-white text-base mb-4 px-2 py-2 border-b border-white rounded-md"
@@ -45,14 +96,15 @@ const AuthForm = ({navigation}) => {
               borderRadius: 4,
               backgroundColor: 'transparent',
             }}
-            selectedValue={'Register as'}
+            onValueChange={inputHandler.bind(this, 'role')}
+            selectedValue={inputValues.role}
             >
             <Picker.Item label='Register as' value={0}/>
             <Picker.Item label='User' value={'User'}/>
             <Picker.Item label='Admin' value={'Admin'}/>
         </Picker> }
       
-      <TouchableOpacity onPress={()=>navigation.navigate('Main Screen')} className="border border-white py-2 rounded-full mt-4">
+      <TouchableOpacity onPress={submitForm} className="border border-white py-2 rounded-full mt-4">
         <Text className="text-white text-center py-2 text-xl tracking-wider font-semibold">{registerScreen ? 'SIGN UP' : 'LOG IN'}</Text>
       </TouchableOpacity>
       
