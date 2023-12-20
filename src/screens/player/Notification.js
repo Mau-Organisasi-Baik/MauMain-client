76d763e21405e66a2d390917666b47db6f0f97db
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2023-12-20 17:45:42 +0700

    fix: chat & accept friend request

--- src/screens/player/Notification.js
+++ src/screens/player/Notification.js
@@ -1,61 +1,79 @@
-import {useState, useContext, useEffect} from 'react';
-import { ScrollView } from 'react-native';
-import Card from '../../components/card/Card';
-import { LoginContext } from '../../context/AuthContext';
-import { BASE_URL } from '../../helpers/BASE_URL';
-import { FriendRequestCard } from '../../components/card/FriendRequestCard';
-import axios from 'axios';
-import TOAST from 'toastify-react-native'
+import { useState, useContext, useEffect, useCallback } from "react";
+import { ScrollView } from "react-native";
+import Card from "../../components/card/Card";
+import { LoginContext } from "../../context/AuthContext";
+import { BASE_URL } from "../../helpers/BASE_URL";
+import { FriendRequestCard } from "../../components/card/FriendRequestCard";
+import { useFocusEffect } from "@react-navigation/native";
+import axios from "axios";
+import TOAST from "toastify-react-native";
 
 const NotificationPopup = () => {
   //get invitation
-  const [invitations, setInvitation] = useState([])
-  const [friendRequest, setFriendRequest] = useState([])
-  const {userInfo} = useContext(LoginContext)
-  const token = userInfo.access_token
-  const getInvitation = async() => {
-      try {
-          const {data} = await axios.get(`${BASE_URL}/invite`, {
-              headers : {
-                  'Authorization' : `Bearer ${token}`
-              }
-          })
-          setInvitation(data.data.invitations)
-      } catch (error) {
-        TOAST.error(error.response.data);
-      }
+  const [invitations, setInvitation] = useState([]);
+  const [friendRequest, setFriendRequest] = useState([]);
+  const { userInfo } = useContext(LoginContext);
+  const token = userInfo.access_token;
+
+  const [indicator, setIndicator] = useState();
+
+  function toggleIndicator() {
+    setIndicator(!indicator);
   }
+
+  const getInvitation = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/invite`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setInvitation(data.data.invitations);
+    } catch (error) {
+      TOAST.error(error.response.data);
+    }
+  };
   //---------
 
   //get friendRequest
-  const getFriendRequest = async() => {
-      try {
-          const {data} = await axios.get(`${BASE_URL}/friends/pending`, {
-              headers : {
-                  'Authorization' : `Bearer ${token}`
-              }
-          })
-          setFriendRequest(data.data.pendings)
-      } catch (error) {
-          throw error
-      }
-  }
+  const getFriendRequest = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/friends/pending`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setFriendRequest(data.data.pendings);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      getFriendRequest();
+      getInvitation();
+      return () => {};
+    }, [])
+  );
+
   useEffect(() => {
-    getFriendRequest()
-    getInvitation()
-  }, [])
+    getFriendRequest();
+    getInvitation();
+  }, [indicator]);
 
   return (
-  <>
-  <ScrollView>
-    {invitations.map(el => <Card key={el._id} invitation={el}/> )}
-    {friendRequest.map(el => <FriendRequestCard key={el._id} setFriendRequest={setFriendRequest} friendRequest={el}/>)}
-    
-  </ScrollView>
-  </>
-  )
+    <>
+      <ScrollView>
+        {invitations.map((el) => (
+          <Card key={el._id} invitation={el} />
+        ))}
+        {friendRequest.map((el) => (
+          <FriendRequestCard key={el._id} friendRequest={el} toggleIndicator={toggleIndicator} />
+        ))}
+      </ScrollView>
+    </>
+  );
 };
-
-
 
 export default NotificationPopup;
