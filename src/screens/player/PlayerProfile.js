36ab57658a5bb3852e5174e12c78188023330aa7
Weneville <wenev.16@gmail.com>
2023-12-20 14:20:49 +0700

    feat: add view player profile

---
+++ src/screens/player/PlayerProfile.js
@@ -0,0 +1,53 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { Button, Image, Text, TouchableOpacity, TextInput } from "react-native";
+import { XPBar } from "../../components/XPBar";
+import { HistoryScroll } from "../../components/HistoryScroll";
+import { LoginContext } from "../../context/AuthContext";
+import { useContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const PlayerProfile = ({ route, navigation }) => {
+  const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
+  const { id } = route.params;
+
+
+const {userInfo} = useContext(LoginContext)
+      const token = userInfo.access_token;
+
+  const [profileData, setProfileData] = useState({
+    profilePictureUrl: "",
+    name: "placeholder",
+    history: [],
+  });
+
+  useEffect(() => {
+    const asyncFn = async () => {
+      
+
+      const { data } = await axios.get(`${BASE_URL}/profile/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setProfileData(data.data.user);
+    };
+    asyncFn();
+  }, []);
+  return (
+    <>
+      <LinearGradient colors={["#003366", "#66CCFF"]} style={{ flex: 1, justifyContent: "center", padding: 16 }}>
+        <TouchableOpacity onPress={() => {}}>
+          <Image className={`rounded-full mx-auto bg-white w-28 h-28`} source={{ uri: profileData.profilePictureUrl }} />
+        </TouchableOpacity>
+        <Text
+          placeholderTextColor="rgba(255, 255, 255, 0.7)"
+          className="text-white text-center text-2xl mt-4"
+        >{profileData.name}</Text>
+
+        <XPBar currentXP={profileData.exp} />
+        <HistoryScroll histories={profileData.history} />
+      </LinearGradient>
+    </>
+  );
+};
