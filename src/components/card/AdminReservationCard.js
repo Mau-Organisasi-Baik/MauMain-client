2296e94af9ce7d043f716018067715775f83c20b
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2023-12-20 13:24:55 +0700

    fix: admin reservation bug

--- src/components/card/AdminReservationCard.js
+++ src/components/card/AdminReservationCard.js
@@ -2,33 +2,17 @@ import { View, Text, TouchableOpacity, ScrollView, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { getScheduleTime } from "../../helpers/ScheduleTime";
 
-export const AdminReservationCard = ({ reservation }) => {
-  const navigation = useNavigation();
-  let detailButton = (
-    <TouchableOpacity
-      onPress={() => navigation.navigate("AdminDetailReservation", { id: reservation.schedule._id })}
-      className="bg-blue-500 py-2 px-4 rounded-full"
-    >
-      <Text className="text-white font-bold">Details</Text>
-    </TouchableOpacity>
-  )
-  if(reservation.status === "empty") {
-    detailButton = (
-      <></>
-    )
-  }
+function EmptyReservation({ reservation, navigation }) {
   return (
     <>
       <View className="mx-4 my-2 p-4 bg-white rounded-lg shadow">
         <View className="flex-row justify-between items-center mb-4">
-          <Text className="text-lg font-bold">{reservation?._id}</Text>
           <Text className="text-sm bg-blue-100 text-blue-800 py-1 px-3 rounded-full">{reservation?.tag?.name}</Text>
         </View>
         <View className="flex-row justify-between items-center">
           <Text className="text-gray-600">
             {reservation.date}, {getScheduleTime(reservation.schedule.TimeStart)} - {getScheduleTime(reservation.schedule.TimeEnd)}
           </Text>
-          {detailButton}
         </View>
         <View className="flex-row">
           <Text className={reservation.status === "ended" ? "bg-red-500 rounded-lg px-2 text-white" : "bg-green-500 rounded-lg px-2 text-white"}>
@@ -36,22 +20,59 @@ export const AdminReservationCard = ({ reservation }) => {
           </Text>
         </View>
         <View className="flex-row mt-4">
-          {reservation.players ? (
-            reservation.players.map((val) => {
-              return (
-                <>
-                  <ScrollView className="">
-                    <Image className="w-12 h-12 rounded-lg" source={{ uri: val.profilePictureUrl }} />
-                    <Text>{val.name ? val?.name : "There's no name"}</Text>
-                  </ScrollView>
-                </>
-              );
-            })
-          ) : (
-            <Text>Theres no reservation!</Text>
-          )}
+          <Text>Theres no reservation!</Text>
         </View>
       </View>
     </>
   );
+}
+
+function NormalReservation({ reservation, navigation }) {
+  return (
+    <>
+      <View className="mx-4 my-2 p-4 bg-white rounded-lg shadow">
+        <View className="flex-row justify-between items-center mb-4">
+          <Text className="text-sm bg-blue-100 text-blue-800 py-1 px-3 rounded-full">{reservation?.tag?.name}</Text>
+        </View>
+        <View className="flex-row justify-between items-center">
+          <Text className="text-gray-600">
+            {reservation.date}, {getScheduleTime(reservation.schedule.TimeStart)} - {getScheduleTime(reservation.schedule.TimeEnd)}
+          </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("AdminDetailReservation", { id: reservation._id })}
+            className="bg-blue-500 py-2 px-4 rounded-full"
+          >
+            <Text className="text-white font-bold">Details</Text>
+          </TouchableOpacity>
+        </View>
+        <View className="flex-row">
+          <Text className={reservation.status === "ended" ? "bg-red-500 rounded-lg px-2 text-white" : "bg-green-500 rounded-lg px-2 text-white"}>
+            {reservation?.status}
+          </Text>
+        </View>
+        <View className="flex-row mt-4">
+          {reservation.players.map((val) => {
+            return (
+              <>
+                <ScrollView className="">
+                  <Image className="w-12 h-12 rounded-lg" source={{ uri: val.profilePictureUrl }} />
+                  <Text>{val.name ? val?.name : "There's no name"}</Text>
+                </ScrollView>
+              </>
+            );
+          })}
+        </View>
+      </View>
+    </>
+  );
+}
+
+export const AdminReservationCard = ({ reservation }) => {
+  const navigation = useNavigation();
+
+  if (reservation.status === "empty") {
+    return <EmptyReservation reservation={reservation} navigation={navigation} />;
+  }
+
+  return <NormalReservation reservation={reservation} navigation={navigation} />;
 };
